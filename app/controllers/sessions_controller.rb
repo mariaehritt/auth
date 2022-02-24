b40938ac11e06a6264fd91a33a992e7f3437cd6d
mariaehritt <maria.ehritt@kellogg.northwestern.edu>
2022-02-24 01:39:28 +0000

    progress at first break

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+    def new
+    end
+
+    def create
+        @user = User.where({ email: params["email"] })[0]
+        if @user
+            if @user.password == params["password"]
+                flash[:notice] = "You logged in!"
+                redirect_to "/companies"
+            else
+                flash[:notice] = "Nope."                
+                redirect_to "/sessions/new"
+            end
+       else
+        flash[:notice] = "Nope."   
+        redirect_to "/sessions/new"
+       end
+    end
+
+end
